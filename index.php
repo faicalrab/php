43e5374e486bb7f40f965ae89d0acaf23f99d6fc
faical rabia <faical.rabiaa@gmail.com>
2019-02-12 12:12:34 +0100

    commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$message = "<h1>le fais du PHP - boucle <em>for</em></h1>";
+
+$message .="<ul>";
+
+for ($i = 0; $i < 11 ; $i++){
+    $message .= "<li>" . $i . "</li>";
+}
+
+$message .="<ul>";
+
+echo $message;
